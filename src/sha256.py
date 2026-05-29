5cd60924894329e8e9694ec892df8e1460658850
Boyzest-Saragih <boyzestsumbayak@gmail.com>
2026-05-29 22:47:25 +0700

    update sigma function for expansi W16...63

--- src/sha256.py
+++ src/sha256.py
@@ -10,6 +10,24 @@ def shiftRight(x,n):
     hasil = (x>>n)&0xFFFFFFFF
     return hasil
 
+#Sigma besar 0 dan 1 untuk mengacak variabel kerja a dan e di setiap ronde kompresi.
+def Sigma1(x):
+    hasil = rightRotate(x,6)^rightRotate(x,11),rightRotate(x,25)
+    return hasil
+
+def Sigma0(x):
+    hasil = rightRotate(x,2)^rightRotate(x,13),shiftRight(x,22)
+    return hasil
+
+# sigma kecil 0 & 1 digunakan untuk ekspansi pesan untuk menghitung nilai $W_{16}$ hingga $W_{63}$
+def sigma0(x):
+    hasil = rightRotate(x,7)^rightRotate(x,18),shiftRight(x,3)
+    return hasil
+
+def sigma1(x):
+    hasil = rightRotate(x,17)^rightRotate(x,19),shiftRight(x,10)
+    return hasil
+
 
 msg = "abcd"
 msg = charToHex(msg)
